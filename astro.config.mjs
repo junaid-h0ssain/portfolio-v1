aa07c9c947ee5d297247ac622d035f761c427b09
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-03-10 20:37:39 +0600

    update to url

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ const isGitHubPagesBuild = process.env.DEPLOY_TARGET === 'github-pages';
 export default defineConfig({
   output: 'static',
   site: 'https://junaid-h0ssain.github.io',
-  base: '/',
+  base: '/portfolio-v1',
   integrations: [react(), mdx()],
 
   vite: {
